93fb49f96e3f02cc833b0f4d067f349b32097f8f
Pierrick Rouxel <pierrick.rouxel@me.com>
2015-05-19 18:09:08 +0200

    refactoring

--- addon/helpers/bs-colorpicker.js
+++ addon/helpers/bs-colorpicker.js
@@ -4,8 +4,6 @@ var $ = Ember.$;
 
 var BsColorpicker = Ember.Object.extend({
 
-  color: Ember.computed.alias('context.color'),
-
   $: function(selector) {
     if (selector) {
       return $(this.get('element')).find(selector);
@@ -15,6 +13,7 @@ var BsColorpicker = Ember.Object.extend({
   },
 
   didInsertElement: function() {
+    this._initializeValues();
     this.$().colorpicker(this.getProperties('color', 'format', 'align'));
     this.$().on('changeColor', $.proxy(this.didChangeColorFromComponent, this));
     this.$().on('showPicker', $.proxy(this.didChangeColorFromComponent, this));
@@ -27,7 +26,7 @@ var BsColorpicker = Ember.Object.extend({
   },
 
   didChangeColorFromComponent: function(event) {
-    var componentColor = event.color.toHex();
+    var componentColor = event.color.toString(this.get('format'));
 
     // Prevent changing color if component is already set
     if (componentColor !== this.get('color')) {
@@ -35,28 +34,57 @@ var BsColorpicker = Ember.Object.extend({
     }
   },
 
-  didChangeColorFromProperty: Ember.observer('color', function() {
+  didChangeColorFromProperty: function() {
     // Prevent changing color if value is already set
     var color = this.$().data('colorpicker').color;
-    if (color.toHex() !== this.get('color')) {
-      color.setColor(this.get('color'));
+    if (this.get('color') !== color.toString(this.get('format'))) {
+      color.setColor(this.get('color') || '#000000');
       this.$().colorpicker('update');
     }
-  })
+  },
+
+  // If value of input is empty, the colorpicker doesn't initialize properly
+  _initializeValues: function() {
+    var color = this.get('color');
+    if (color) {
+      if (this.$().is('input')) {
+        this.$().val(color);
+      } else if (this.$('input').length) {
+        this.$('input').val(color);
+      }
+    }
+    this.$().data('color', color);
+  },
 
 });
 
 export default function(options) {
   var hash = options.hash;
   var element = options.element;
-  var context = options.data.view.get('context');
+  var view = options.data.view;
+  var stream = view.getStream(hash.color);
 
   var colorpicker = BsColorpicker.create({
     element: element,
-    context: context,
     format: hash.format,
     align: hash.align
   });
+
+  // Add computed property for color
+  Ember.defineProperty(colorpicker, 'color', Ember.computed({
+    set: function(key, value) {
+      stream.setValue(value);
+      return value;
+    },
+    get: function() {
+      return stream.value();
+    }
+  }).volatile());
+
+  stream.subscribe(function() {
+    colorpicker.didChangeColorFromProperty();
+  });
+
   colorpicker.didInsertElement();
 
   options.data.view.on('willDestroyElement', function() {
