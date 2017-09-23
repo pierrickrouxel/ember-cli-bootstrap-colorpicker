852ab916284c4fe947a7b7e0611370ca9031a278
Pierrick Rouxel <pierrick.rouxel@me.com>
2017-09-23 12:25:39 +0200

    Import dependencies from npm

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-cli-bootstrap-colorpicker',
 
-  included: function(app) {
+  included(app) {
     this.app = app;
 
     this._super.included(app);
-
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css');
+    
     app.import('vendor/ember-cli-bootstrap-colorpicker/bs-colorpicker.css');
 
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/img/bootstrap-colorpicker/alpha-horizontal.png', { destDir: 'img/bootstrap-colorpicker' });
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/img/bootstrap-colorpicker/alpha.png', { destDir: 'img/bootstrap-colorpicker' });
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/img/bootstrap-colorpicker/hue-horizontal.png', { destDir: 'img/bootstrap-colorpicker' });
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/img/bootstrap-colorpicker/hue.png', { destDir: 'img/bootstrap-colorpicker' });
-    app.import(app.bowerDirectory + '/mjolnic-bootstrap-colorpicker/dist/img/bootstrap-colorpicker/saturation.png', { destDir: 'img/bootstrap-colorpicker' });
-  }
+    let vendorPath = path.join('vendor', 'bootstrap-colorpicker');
+    app.import(path.join(vendorPath, 'bootstrap-colorpicker.css'));
+    app.import(path.join(vendorPath, 'bootstrap-colorpicker.js'));
+  },
+
+  getColorpickerStylesPath() {
+    let nodeModulesPath = this.app.project.nodeModulesPath;
+    return path.join(nodeModulesPath, 'bootstrap-colorpicker', 'dist', 'css');
+  },
+
+  getColorpickerJavascriptsPath() {
+    let nodeModulesPath = this.app.project.nodeModulesPath;
+    return path.join(nodeModulesPath, 'bootstrap-colorpicker', 'dist', 'js');
+  },
+
+  getColorpickerImagePath() {
+    let nodeModulesPath = this.app.project.nodeModulesPath;
+    return path.join(nodeModulesPath, 'bootstrap-colorpicker', 'dist', 'img');
+  },
+
+  treeForVendor(tree) {
+    let trees = tree ? [tree] : [];
+    trees.push(new Funnel(this.getColorpickerStylesPath(), {
+      destDir: 'bootstrap-colorpicker'
+    }));
+    trees.push(new Funnel(this.getColorpickerJavascriptsPath(), {
+      destDir: 'bootstrap-colorpicker'
+    }));
+    return mergeTrees(trees, { overwrite: true });
+  },
+
+  treeForPublic() {
+    return new Funnel(this.getColorpickerImagePath(), {
+      destDir: 'img'
+    });
+  },
 };
